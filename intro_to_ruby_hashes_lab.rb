93866178145b5488a21c3c79004dcb62b17c52d8
Simon Tucker <sjtucker28@gmail.com>
2019-12-16 16:07:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -48,13 +48,11 @@ def monopoly_with_fourth_tier
         :four_pieces_owned => 200
       },
       :names => {
-        :reading_railroad => {
-          :mortage_value => "$"
-        },
+        :reading_railroad => {},
         :pennsylvania_railroad => {},
         :b_and_o_railroad => {},
         :shortline_railroad => {}
-      }
+      },
     }
   }
   
